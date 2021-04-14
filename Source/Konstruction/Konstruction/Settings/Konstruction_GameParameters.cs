dc5e9f9b6cc65b760e279673dde89bf18fbc09b7
tjdeckard <tjdeckard@users.noreply.github.com>
2021-04-13 21:15:07 -0400

    Add a game parameter to configure the range for local resource transfers

---
+++ Source/Konstruction/Konstruction/Settings/Konstruction_GameParameters.cs
@@ -0,0 +1,35 @@
+﻿using static GameParameters;
+
+namespace Konstruction
+{
+    public class Konstruction_GameParameters : CustomParameterNode
+    {
+        public override bool HasPresets => false;
+        public override string DisplaySection => "Konstruction";
+        public override GameMode GameMode => GameMode.ANY;
+        public override string Section => "Konstruction";
+        public override int SectionOrder => 1;
+        public override string Title => string.Empty;
+
+        #region Custom UI parameters
+        [CustomIntParameterUI(
+            "#LOC_USI_ResourceTransfers_AllowedRadius_OptionTitle",
+            autoPersistance = true, minValue = 50, maxValue = 1000, stepSize = 50,
+            toolTip = "#LOC_USI_ResourceTransfers_AllowedRadius_Tooltip")]
+        public int resourceTransferAllowedRadius = 250;
+        #endregion
+
+        #region Static accessor properties
+        public static float ResourceTransferAllowedRadius
+        {
+            get
+            {
+                var options = HighLogic.CurrentGame.Parameters
+                    .CustomParams<Konstruction_GameParameters>();
+
+                return options.resourceTransferAllowedRadius;
+            }
+        }
+        #endregion
+    }
+}
